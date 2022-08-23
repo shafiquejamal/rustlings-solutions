9a39aa3f81b15b00e265dda4c4d75c41a31fd58b
Shafique Jamal <shafique.jamal@gmail.com>
2022-08-23 08:26:41 -0400

    Solve (most) exercises.

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,25 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            Person::default()
+        } else {
+            let elements: Vec<_> = s.split(",").collect();
+            if elements.len() != 2 {
+                return Person::default();
+            }
+            let name = elements[0].to_string();
+            if name.len() == 0 {
+                return Person::default();
+            }
+            let maybe_age = elements[1].parse::<usize>();
+            match maybe_age {
+                Ok(age) => Person { name, age },
+                _ => Person::default(),
+            }
+        }
     }
 }
 
